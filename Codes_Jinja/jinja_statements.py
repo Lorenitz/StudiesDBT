0b0f6f65b37214d67824c6510acc213113fa9ebe
Lorenitz <lorena.g.saar@gmail.com>
2023-04-16 16:52:28 +0200

    jinja if statement example code

---
+++ Codes_Jinja/jinja_statements.py
@@ -0,0 +1,11 @@
+{% set temperature = 45 %}
+
+
+{% if temperature < 65 %}
+   Time for a capuccino
+{% else %}
+   Time for a cold brew
+{% endif %}   
+   
+# The result for this example is going to be Time for a cold brew  
+
